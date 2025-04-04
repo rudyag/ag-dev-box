2bd1921f00b1e529d16307154b190a4bf684bba3
Rudy <rlartey@agiliteksolutions.com>
2025-04-04 16:39:02 -0600

    Update install-tools-unattended.ps1

--- install-tools-unattended.ps1
+++ install-tools-unattended.ps1
@@ -1,17 +1,17 @@
-# Install Slack
+Write-Host "`nInstalling Slack..."
 winget install -e --id SlackTechnologies.Slack
 
-# Install Linear
+Write-Host "`nInstalling Linear..."
 winget install -e --id LinearOrbit.Linear
 
-# Install Bruno
+Write-Host "`nInstalling Bruno..."
 winget install --id=Bruno.Bruno -e
 
-# Install DBeaver
+Write-Host "`nInstalling DBeaver..."
 winget install --id=dbeaver.dbeaver -e
 
-# Install Python
+Write-Host "`nInstalling Python.3.12..."
 winget install --id=Python.Python.3.12 -e
 
-# Install Just
+Write-Host "`nInstalling Just..."
 winget install --id=Casey.Just -e
